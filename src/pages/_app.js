e3c658f9fce37a7cb6bf0eb66d37f8d8ef9aaa2c
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-04-04 13:19:14 +0300

    add try catch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+// import Image from "next/image";
+// import youtubeImg from "../../public/youtube.png";
+import Layout from "@/components/Layout";
+import "@/styles/globals.scss";
+// import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      {/* <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </Head> */}
+      <main>
+        <Component {...pageProps} />
+      </main>
+      {/* <Image
+        src={youtubeImg}
+        width={800}
+        alt="youtubeImg"
+        placeholder="blue"
+        priority="false"
+      /> */}
+    </Layout>
+  );
 }
